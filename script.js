117782e31005a43f48632034f879a841a61133e9
Kabz Jose <josekabz00@gmail.com>
2025-10-04 16:08:48 +0300

    Hosting my website

--- script.js
+++ script.js
@@ -1,98 +1,116 @@
-let input1=document.getElementById("admno")
-let input2=document.getElementById("name")
-let input3=document.getElementById("score")
-let button=document.getElementById("btn")
-
-
+let input1 = document.getElementById("admno");
+let input2 = document.getElementById("name");
+let input3 = document.getElementById("score");
+let button = document.getElementById("btn");
 
 let students = JSON.parse(localStorage.getItem("students")) || [];
 
 //addding students
 
-function addStudent(){
-    if (input1.value.trim() === "" || input2.value.trim() === "" ||input3.value.trim() === "")  return;//prevent empty inputs
-    
-    let name = input2.value.trim();//remove any spaces
-    let score = Number(input3.value.trim());
-    let admission = String(input1.value.trim()); 
+function addStudent() {
+  if (
+    input1.value.trim() === "" ||
+    input2.value.trim() === "" ||
+    input3.value.trim() === ""
+  )
+    return; //prevent empty inputs
 
+  let name = input2.value.trim(); //remove any spaces
+  let score = Number(input3.value.trim());
+  let admission = String(input1.value.trim());
 
-    // Validate empty fields
-  if (name === "") {
-    alert("Please enter name.");
+  // allow only alphabets and spaces
+  if (!/^[A-Za-z\s]+$/.test(name)) {
+    alert("Name should only contain letters.");
     return;
-}
+  }
 
   // Admission must be exactly 5 digits
   if (admission.length !== 5 || isNaN(admission)) {
-  alert("Admission number must be exactly 5 digits");
-  return; // stop adding student
-}
-  
+    alert("Admission number must be exactly 5 digits");
+    return; // stop adding student
+  }
+
   // Validate score
   if (score < 0 || score > 100) {
     alert("Score must be between 0 and 100.");
     return;
   }
 
-     students.push({
-        admno:input1.value,
-        name:input2.value,
-        score:input3.value
-     });
-     localStorage.setItem("students", JSON.stringify(students));
-    input1.value=""
-   input2.value=""
-   input3.value=""
-   renderStudents();
+  students.push({
+    admno: input1.value,
+    name: input2.value,
+    score: input3.value,
+  });
+  localStorage.setItem("students", JSON.stringify(students));
+  input1.value = "";
+  input2.value = "";
+  input3.value = "";
+  renderStudents();
 }
 
+button.addEventListener("click", addStudent);
 
-button.addEventListener("click",addStudent)
+function renderStudents() {
+  let tbody = document.getElementById("tablebody");
+  tbody.innerHTML = ""; //clear existing rows
 
+  //looping through students array and creating rows
+  students.forEach((student, index) => {
+    let tr = document.createElement("tr");
+    //Admission number cell
+    let admnotd = document.createElement("td");
+    admnotd.innerText = student.admno;
+    tr.appendChild(admnotd);
+    //name the cell
+    let nametd = document.createElement("td");
+    nametd.innerText = student.name;
+    tr.appendChild(nametd);
+    //score cell
+    let scoretd = document.createElement("td");
+    scoretd.innerText = student.score;
+    tr.appendChild(scoretd);
+    //Edit button
+    let editBtn = document.createElement("button");
+    editBtn.innerText = "Edit";
+    actiontd.appendChild(editBtn);
+    editBtn.addEventListener("click", () => {
+      let newName = prompt("Enter new name:", student.name);
+      let newScore = prompt("Enter new score:", student.score);
 
+      if (newName && newScore !== null) {
+        students[index].name = newName;
+        students[index].score = Number(newScore);
+        addStudent();
+        renderStudents();
+      }
+    });
 
-function renderStudents(){
-    let tbody=document.getElementById("tablebody")
-    tbody.innerHTML=""//clear existing rows
+    //Action cell -delete button
+    let actiontd = document.createElement("td");
+    let deletebtn = document.createElement("button");
+    deletebtn.innerText = "Delete";
+    deletebtn.addEventListener("click", () => {
+      let confirmation = confirm(
+        `Are you sure you want to delete ${student.name}?`
+      ); //Confirm before deleting aa student
+      if (confirmation) {
+        students.splice(index, 1); //remove from array
+        localStorage.setItem("students", JSON.stringify(students)); //update storage
+        renderStudents(); //refresh table
+      }
+    });
+    actiontd.append(deletebtn);
+    tr.appendChild(actiontd);
 
-    //looping through students array and creating rows
-    students.forEach((student,index)=>{
-        let tr =document.createElement("tr")
-        //Admission number cell
-        let admnotd=document.createElement("td")
-        admnotd.innerText=student.admno
-        tr.appendChild(admnotd)
-        //name the cell
-        let nametd=document.createElement("td")
-        nametd.innerText=student.name
-        tr.appendChild(nametd)
-        //score cell
-        let scoretd=document.createElement("td")
-        scoretd.innerText=student.score
-        tr.appendChild(scoretd)
-        //Action cell -delete button
-        let actiontd=document.createElement("td")
-        let deletebtn=document.createElement("button")
-        deletebtn.innerText="Delete"
-        deletebtn.addEventListener("click",()=>{
-            let confirmation = confirm(`Are you sure you want to delete ${student.name}?`);//Confirm before deleting aa student
-  if (confirmation) {
-            students.splice(index,1)//remove from array
-            localStorage.setItem("students",JSON.stringify(students))//update storage
-            renderStudents()  //refresh table
-        }
-        });
-        actiontd.append(deletebtn)
-        tr.appendChild(actiontd)
-        
-        //add row to table
-        tbody.appendChild(tr)
-    })
+    //add row to table
+    tbody.appendChild(tr);
+  });
 }
 renderStudents();
 
-document.addEventListener("keydown", (e) => {//when you press enter the addStudent() executes 
+document.addEventListener("keydown", (e) => {
+  //when you press enter the addStudent() executes
   if (e.key === "Enter") addStudent();
 });
 //Added arrow keys to navigate swiftly
@@ -100,7 +118,7 @@ let inputs = [
   document.getElementById("admno"),
   document.getElementById("name"),
   document.getElementById("score"),
-  document.getElementById("btn") 
+  document.getElementById("btn"),
 ];
 
 inputs.forEach((input, index) => {
@@ -119,4 +137,3 @@ inputs.forEach((input, index) => {
     }
   });
 });
-
